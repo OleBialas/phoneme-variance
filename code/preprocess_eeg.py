4f52661b9b46199d2b4fae74ea53b792caadfc61
obi <ole.bialas@posteo.de>
2023-01-23 14:55:12 -0500

    no ica

--- code/preprocess_eeg.py
+++ code/preprocess_eeg.py
@@ -1,13 +1,11 @@
 from pathlib import Path
 import numpy as np
 from mne.io import read_raw_brainvision
-from mne.preprocessing import ICA, read_ica, corrmap
 from pyprep.ransac import find_bad_by_ransac
 from mne.channels import make_standard_montage
 
 root = Path(__file__).parent.parent.absolute()
 
-reference = read_ica(root / "code" / "blink_reference-ica.fif")
 montage = make_standard_montage("biosemi128")
 positions = np.stack([ch for ch in montage.get_positions()["ch_pos"].values()])
 
@@ -44,15 +42,5 @@ for subfolder in (root / "raw").glob("sub*"):
         )
         raw.info["bads"] = bads
         raw.interpolate_bads()
-        ica = ICA(n_components=0.99)
-        ica.fit(raw)
-        corrmap(
-            [reference, ica],
-            template=(0, component[0]),
-            label="blinks",
-            plot=False,
-            threshold=0.8,
-        )
-        ica.apply(raw, exclude=ica.labels_["blinks"])
+        raw.set_eeg_reference("average")
         raw.save(out_folder / (run.name.split("_eeg")[0] + "-raw.fif"), overwrite=True)
-        ica.save(out_folder / (run.name.split("_eeg")[0] + "-ica.fif"), overwrite=True)
