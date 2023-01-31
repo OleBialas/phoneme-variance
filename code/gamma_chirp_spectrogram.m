ffc0e06be5a29eac42c91d611a9d4ccaa1104678
obi <ole.bialas@posteo.de>
2023-01-31 15:18:08 -0500

    spectrogram

---
+++ code/gamma_chirp_spectrogram.m
@@ -0,0 +1,62 @@
+% Extract envelope using Gammachirp filterbank 
+addpath('./GCFBv210pack/')
+%% Set up parameters
+wav_fs=44100; % Sampling frequency of source audio files
+down_factor=1; % envelope downsampling factor
+
+% Parameters for lowpass filter
+Fpass = 9e3;
+Fstop = 11e3;
+Fs=wav_fs;
+Apass = 1;
+Astop = 60;
+h = fdesign.lowpass(Fpass,Fstop,Apass,Astop,Fs);
+lpf1 = design(h,'cheby2','MatchExactly','stopband');
+clear Fpass Fstop Fs h;
+
+% GammaChirp filterbank
+GCparam.fs = wav_fs/down_factor; 
+GCparam.NumCh = 16;
+GCparam.FRange = [80,8e3];
+GCparam.OutMidCrct = 'ELC';
+% GCparam.OutMidCrct = 'No';
+% GCparam.Ctrl = 'dyn';
+
+% Envelope filter
+Fpass = 30;
+Fstop = 32;
+Fs = GCparam.fs;
+h = fdesign.lowpass(Fpass,Fstop,Apass,Astop,Fs);
+lpf2 = design(h,'cheby2','MatchExactly','stopband');
+clear Fpass Fstop Fs h;
+
+%% Envelope extraction 
+folders = char('single_speaker', 'multi_speaker');
+for f = 1:2
+    wavs = dir(strcat('../raw/stimuli/', folders(f, :), '/*.wav'));
+    nwavs = length(wavs);
+    envelopes={};
+    for i = 1:nwavs
+
+        % Read in audio
+        f_name = fullfile(wavs(i).folder, wavs(i).name)
+        [y,Fs] = audioread(f_name);
+        assert(Fs==wav_fs);
+        y=(y(:,1)); % only keep 1st channel
+        y = filtfilthd(lpf1,y); % Filter below Nyquist frequency
+        y= nt_dsample(y,Fs/GCparam.fs); % Downsample
+        y=y';
+        spectrogram = GCFBv210(y,GCparam); % Bandpass filter
+
+        for chn=1:size(spectrogram,1) % envelope of each spectrogram band
+            spectrogram(chn,:)=abs(hilbert(spectrogram(chn,:)));
+        end
+
+        envelope = mean(spectrogram,1); % broadband envelope
+        envelope = filtfilthd(lpf2,envelope); % lowpass filter
+        
+        save(fullfile(wavs(i).folder, strcat(wavs(i).name(1:end-4), '_env.mat')), 'envelope', 'Fs');
+        save(fullfile(wavs(i).folder, strcat(wavs(i).name(1:end-4),'_spg.mat')), 'spectrogram', 'Fs');
+    end
+end
+
