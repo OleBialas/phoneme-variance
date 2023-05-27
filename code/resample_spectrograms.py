f1f34aa6423a1993f17bff2a180a72497ce72bf7
obi <ole.bialas@posteo.de>
2023-05-26 22:23:52 -0400

    [DATALAD RUNCMD] matlab -nodesktop -nosplash -r "gamma_ch...
    === Do not change lines below ===
    {
     "chain": [],
     "cmd": "matlab -nodesktop -nosplash -r \"gamma_chirp_spectrogram;exit\"",
     "dsid": "8b828116-eb3e-4fe2-85ec-cb1c5ca60916",
     "exit": 0,
     "extra_inputs": [],
     "inputs": [],
     "outputs": [],
     "pwd": "code"
    }
    ^^^ Do not change lines above ^^^

--- code/resample_spectrograms.py
+++ code/resample_spectrograms.py
@@ -15,3 +15,16 @@ for s in spectrograms:
     spg = slab.Sound(spg, samplerate=fs)
     spg = spg.filter(int(128 / 3), kind="lp")
     spg = spg.resample(128)
+    spg.write(s.parent / f'{s.name.split(".")[0]}.wav')
+
+spectrograms = list(
+    (root / "results" / "spectrograms" / "multi_speakers").glob("*_spg.mat")
+)
+
+for s in spectrograms:
+    mat = loadmat(s)
+    spg, fs = mat["spectrogram"], mat["Fs"][0][0]
+    spg = slab.Sound(spg, samplerate=fs)
+    spg = spg.filter(int(128 / 3), kind="lp")
+    spg = spg.resample(128)
+    spg.save()
