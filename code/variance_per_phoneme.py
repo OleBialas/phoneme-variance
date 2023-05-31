e1b61cd3a045b70ecb829534e1a58f7cab5b2368
obi <ole.bialas@posteo.de>
2023-05-31 10:34:56 -0400

    compute phoneme variance

--- code/variance_per_phoneme.py
+++ code/variance_per_phoneme.py
@@ -11,7 +11,8 @@ phoneme_codes = np.asarray(
     list(json.load(open(root / "code" / "phoneme_codes.json")).keys())
 )
 
-tmp_var, amp_var, count = [], []
+tmp_var, amp_var, count = [], [], []
+tmp_var, amp_var, count = [], [], []
 for pc in phoneme_codes:
     warping = np.load(
         root / "results" / "aligned" / "single_speaker" / f"{pc}_warping.npy"
@@ -19,9 +20,25 @@ for pc in phoneme_codes:
     spectrograms = np.load(
         root / "results" / "aligned" / "single_speaker" / f"{pc}_spectrograms.npy"
     )
-    tmp_var.append(np.var(warping))
-    amp_var.append(np.var(spectrograms))
+    # compute the variance across utterances, then average across time (and bands)
+    tmp_var.append(warping.var(axis=0).mean())
+    amp_var.append(spectrograms.var(axis=1).mean())
+    count.append(spectrograms.shape[0])
+for pc in phoneme_codes:
+    warping = np.load(
+        root / "results" / "aligned" / "single_speaker" / f"{pc}_warping.npy"
+    )
+    spectrograms = np.load(
+        root / "results" / "aligned" / "single_speaker" / f"{pc}_spectrograms.npy"
+    )
+    # compute the variance across utterances, then average across time (and bands)
+    tmp_var.append(warping.var(axis=0).mean())
+    amp_var.append(spectrograms.var(axis=1).mean())
     count.append(spectrograms.shape[0])
 
 # save as csv
-data = np.stack([tmp_var, amp_var, count])
+data = np.stack([tmp_var, amp_var, count, phoneme_codes])
+df = pd.DataFrame(
+    data=data.T, columns=["temporal_variance", "amplitude_variance", "count", "phoneme"]
+)
+df.to_csv(root / "results" / "variance_per_phoneme.csv")
