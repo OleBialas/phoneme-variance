4cbc1d86979f973f69f750037191cb13afb41408
Ole <ole.bialas@posteo.de>
2023-07-05 16:13:01 -0400

    [DATALAD] Recorded changes

--- code/single_speaker_trfs.py
+++ code/single_speaker_trfs.py
@@ -2,107 +2,73 @@ from pathlib import Path
 import json
 import numpy as np
 import pandas as pd
-from mne.io import read_raw_fif
+from scipy.io import loadmat
+from mne.io import read_raw_brainvision
+from mne.channels import make_standard_montage
 import textgrid
-import slab
 from mtrf.model import TRF, cross_validate
 
 root = Path(__file__).parent.parent.absolute()
+montage = make_standard_montage("biosemi128")
+positions = np.stack([ch for ch in montage.get_positions()["ch_pos"].values()])
 cfg = json.load(open(root / "code" / "trf_parameters.json"))
 phoneme_codes = np.asarray(
     list(json.load(open(root / "code" / "phoneme_codes.json")).keys())
 )
-
-
 text_grids = list((root / "raw" / "stimuli" / "single_speaker").glob("*TextGrid"))
-spectrograms = list(
-    (root / "results" / "spectrograms" / "single_speaker").glob("*_spg.wav")
-)
-text_grids.sort(), spectrograms.sort()
+mat_files = list((root / "results" / "spectrograms" / "single_speaker").glob("*.mat"))
+text_grids.sort(), mat_files.sort()
 
 # get spectograms and phoneme stick functions
-pho, spg = [], []
-for t, s in zip(text_grids, spectrograms):
-    sound = slab.Sound(s)
-    fs, sound = sound.samplerate, sound.data
-    sound = (sound - sound.mean(axis=0)) / sound.std(axis=0)
-    spg.append(sound)
+phonemes, spectrograms = [], []
+for t, m in zip(text_grids, mat_files):
+    mat = loadmat(m)
+    spg, fs = mat["spectrogram"], mat["Fs"][0][0]
+    spectrograms.append((spg - spg.mean(axis=0)) / spg.std(axis=0))
     phoneme_grid = textgrid.TextGrid.fromFile(t)[0]
-    pho.append(np.zeros((spg[-1].shape[0], len(phoneme_codes))))
+    phonemes.append(np.zeros((spectrograms[-1].shape[0], len(phoneme_codes))))
     for p in phoneme_grid:
         if p.mark[:2] in phoneme_codes:
             idx = np.where(np.asarray(phoneme_codes) == p.mark[:2])[0][0]
             start = round(p.minTime * fs)
             stop = round(p.maxTime * fs)
-            pho[-1][start:stop, idx] = 1
-# stack spectrogram and phonemes into one vector
-spg_pho = []
-for s, p in zip(spg, pho):
-    spg_pho.append(np.concatenate([s, p], axis=1))
-# also add the onsets
-ons = []
-for s in spg:
+            phonemes[-1][start:stop, idx] = 1
+onsets = []
+for s in spectrograms:
     o = np.diff(s.mean(axis=1, keepdims=True), prepend=np.zeros((1, 1)), axis=0).clip(
         min=0
     )
-    ons.append((o - o.mean()) / o.std())
-spg_pho_ons = []
-for s, o in zip(spg_pho, ons):
-    spg_pho_ons.append(np.concatenate([o, s], axis=1))
+    onsets.append((o - o.mean()) / o.std())
+stimulus = []
+for s, o, p in zip(spectrograms, onsets, phonemes):
+    stimulus.append(np.concatenate([s, o, p], axis=1))
 
-# compute spg and spg+pho trf for each subject
-subjects = list((root / "preprocessed").glob("sub-0*"))
-for sub in subjects:
-    responses = []
-    runs = list(sub.glob("*.fif"))
-    runs.sort()
-    for irun, run in enumerate(runs):
-        raw = read_raw_fif(run, verbose=False)
-        responses.append(raw.get_data().T[0 : spg[irun].shape[0], :])
+subjects = list((root / "raw").glob("sub-0*"))
+for subject in subjects:
+    response = []
+    recordings = list((subject / "eeg").glob("*_eeg.vhdr"))
+    channels = list((subject / "eeg").glob("*_channels.tsv"))
+    recordings.sort(), channels.sort()
+    for rec, chs, stm in zip(recordings, channels, stimulus):
+        raw = read_raw_brainvision(rec, verbose=False, preload=True)
+        raw.set_montage(montage)
+        bads = (pd.read_csv(chs, sep="\t").status == "bad").tolist()
+        raw.info["bads"] = [
+            ch for ch, bad in zip(raw.info["ch_names"], bads) if bad is True
+        ]
+        raw = raw.interpolate_bads()
+        raw = raw.filter(1, 20)
+        raw = raw.resample(fs)
+        raw = raw.set_eeg_reference("average")
+        response.append(raw.get_data().T[0 : stm.shape[0], :])
 
-    trf_pho = TRF()
-    trf_pho.train(
-        pho,
-        responses,
+    trf = TRF()
+    trf.train(
+        stimulus,
+        response,
         raw.info["sfreq"],
         cfg["tmin"],
         cfg["tmax"],
         cfg["lambda"],
     )
-    trf_pho.save(root / "results" / "trfs" / f"{sub.name}_pho.trf")
-    del trf_pho
-
-    trf_spg = TRF()
-    trf_spg.train(
-        spg,
-        responses,
-        raw.info["sfreq"],
-        cfg["tmin"],
-        cfg["tmax"],
-        cfg["lambda"],
-    )
-    trf_spg.save(root / "results" / "trfs" / f"{sub.name}_spg.trf")
-    del trf_spg
-
-    trf_spg_pho = TRF()
-    trf_spg_pho.train(
-        spg_pho,
-        responses,
-        raw.info["sfreq"],
-        cfg["tmin"],
-        cfg["tmax"],
-        cfg["lambda"],
-    )
-    trf_spg_pho.save(root / "results" / "trfs" / f"{sub.name}_spg_pho.trf")
-    del trf_spg_pho
-
-    trf_spg_pho_ons = TRF()
-    trf_spg_pho_ons.train(
-        spg_pho_ons,
-        responses,
-        raw.info["sfreq"],
-        cfg["tmin"],
-        cfg["tmax"],
-        cfg["lambda"],
-    )
-    trf_spg_pho_ons.save(root / "results" / "trfs" / f"{sub.name}_spg_pho_ons.trf")
+    trf.save(root / "results" / "trfs" / f"{sub.name}_pho.trf")
